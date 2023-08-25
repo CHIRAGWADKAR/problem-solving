0a05a8685071c94bf5aa8bc3973cff0fd2163a79
CHIRAG WADKAR <89576084+CHIRAGWADKAR@users.noreply.github.com>
2023-08-26 01:20:47 +0530

    Added the files to the problems-solving repository.

---
+++ Two_Sum_LTCP1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int target=9;
+    int arr[] = {2, 7, 11, 15};
+    int len = sizeof(arr)/sizeof(arr[0]);
+
+    for(int i=0;i<len;i++) {
+        for(int j=i+1;j<len;j++) {
+            if(arr[i]+arr[j] == target) {
+                cout << "[" << i << "," << j << "]";
+            }
+        }
+    }
+
+    return -1;
+}
